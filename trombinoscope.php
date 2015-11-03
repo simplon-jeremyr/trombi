0bb9a9b81d8bebe4a31b06ea77a53505ae63070b
jeremy11 <j.raynaud@outlook.com>
2015-11-03 14:01:43 +0100

    fichier trombinoscope

---
+++ trombinoscope.php
@@ -0,0 +1,92 @@
+<DOC!TYPE html>
+<html>
+<header>
+			<div id="titre_1">
+			<h2>Exercice 02.11.2015</h2>
+			</div>
+			<div id="titre_2">
+			<h2>HTML / CSS</h2>
+			</div>
+		</header>
+
+		<div id="cadre_logo">
+			<img src="./Images/logo_simplon" alt="Logo Simplon" id="logo" />
+		</div>
+
+		<div id="banniere_image">
+			<div id="banniere_texte">
+				<h1>SIMPLON.CO</h1>
+				<p>Simplon Narbonne</p>
+				<p>Promo 2015.2016</p>
+			</div>
+		</div>
+
+		<html lang="fr">
+
+		  <head>
+			<meta charset="UTF-8">
+			<link rel="stylesheet" href="indexben2.css" />
+		        <!--[if lt IE 9]>
+		            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		        <![endif]-->
+			<script src="https://raw.github.com/LeaVerou/prefixfree/gh-pages/prefixfree.min.js"></script> <!-- Fichier JavaScript assure support CSS3   -->
+		  </head>
+			<body>
+
+		    <?php
+		    include("entete.php");
+		     ?>
+
+
+			  <div id="galerie-menu">
+				<nav>
+		      <a href="#" tabindex="0" title="Alexis CHALONO"><img src="Photos/Alexis_CHALONO.JPG"  alt="Alexis CHALONO"  />
+		      </a><a href="#" tabindex="0" title="Aline ELWERT"><img src="Photos/Aline_ELWERT.JPG"  alt="Aline ELWERT"  />
+					</a><a href="#" tabindex="0" title="Amandine MICHEL"><img src="Photos/Amandine_MICHEL.JPG"  alt="Amandine MICHEL"  />
+					</a><a href="#" tabindex="0" title="Benjamin BULLICH"><img src="Photos/Benjamin_BULLICH.JPG"  alt="Benjamin_BULLICH"  />
+					</a><a href="#" tabindex="0" title="Catherine STRUGALA"><img src="Photos/Catherine_STRUGALA.JPG"  alt="Catherine STRUGALA"  />
+					</a><a href="#" tabindex="0" title="Edouard GRAVEZ"><img src="Photos/Edouard_GRAVEZ.JPG"  alt="Edouard GRAVEZ"  />
+					</a><a href="#" tabindex="0" title="Gregory GUILLAUME"><img src="Photos/Gregory_GUILLAUME.JPG"  alt="Gregory GUILLAUME"  />
+					</a><a href="#" tabindex="0" title="Gwenaelle GUEZOU"><img src="Photos/Gwenaelle_GUEZOU.JPG"  alt="Gwenaelle GUEZOU"  />
+					</a><a href="#" tabindex="0" title="Isabelle Gomes Da Costa"><img src="Photos/Isabelle_Gomes_Da_Costa.jpg"  alt="Isabelle Gomes Da Costa"  />
+
+					</a><a href="#" tabindex="0" title="Jeremy RAYNAUD"><img src="Photos/Jeremy_raynaud.JPG" alt="Jeremy RAYNAUD" />
+		      </a><a href="#" tabindex="0" title="Jerome NANGUET"><img src="Photos/Jerome_NANGUET.JPG" alt="Jerome NANGUET" />
+		      </a><a href="#" tabindex="0" title="Julie Michelet"><img src="Photos/Julie_Michelet.JPG" alt="Julie Michelet" />
+		      </a><a href="#" tabindex="0" title="Nicolas Devynck"><img src="Photos/Nicolas_Devynck.JPG" alt="Nicolas Devynck" />
+		      </a><a href="#" tabindex="0" title="Otman HANAN"><img src="Photos/Otman_Hanan.JPG" alt="Otman HANAN" />
+		      </a><a href="#" tabindex="0" title="Patrick BELAIRE"><img src="Photos/Patrick_BELAIRE.JPG" alt="Patrick BELAIRE" />
+		      </a><a href="#" tabindex="0" title="Patrick MORRGHADI"><img src="Photos/Patrick_Morghadi.JPG" alt="Patrick MORRGHADI" />
+		      </a><a href="#" tabindex="0" title="Philippe Colas"><img src="Photos/Philippe_Colas.jpg" alt="Philippe Colas" />
+		      </a><a href="#" tabindex="0" title="Thomas NGOMA"><img src="Photos/Thomas_NGOMA.JPG" alt="Thomas NGOMA" />
+
+		      </a><a href="#" tabindex="0" title="Tyffany ZILLIOX"><img src="Photos/Tyffany_ZILLIOX.JPG" alt="Tyffany ZILLIOX" />
+		      </a><a href="#" tabindex="0" title="Ulysse GABELLI"><img src="Photos/Ulysse_GABELLI.JPG" alt="Ulysse GABELLI" />
+		      </a><a href="#" tabindex="0" title="Renaud VIVANCOS"><img src="Photos/VIVANCOS_Renaud.JPG" alt="Renaud VIVANCOS" />
+		      </a><a href="#" tabindex="0" title="David TARCZEWSKI"><img src="Photos/david_TARCZEWSKI.JPG" alt="David TARCZEWSKI" />
+		      </a><a href="#" tabindex="0" title="Emmanuel DELOS"><img src="Photos/delos_emmanuel.JPG" alt="Emmanuel DELOS" />
+				</a></nav>
+			  </div>
+
+		    <div class="fin">
+		  <?php
+		  include("footer.php");
+		   ?>
+		  </div>
+			</body>
+
+		<footer>
+			<div class="fin">
+				<div class="fin1">
+			<a href="http://simplon.co/">	<img src="http://ulule.me/presales/5/6/6/10665/logo_simplonco_rect_coul_blanc400px_png_640x860_q85.jpg" alt="Nouveau!" /></a>
+			</div>
+				<div class="fin3">
+				<a href="http://entreprendre.legrandnarbonne.com/717-iness.html"><img src="http://entreprendre.legrandnarbonne.com/uploads/pics/img_INESS.jpg" alt="Nouveau!" /></a>
+		</div>
+		<div class="rageux">
+			<p><h6 align="center">realised by Benjamin Bullich , Thomas Ngoma , Edouard Gravez	, Jeremy Raynaud  						</h6></p>
+
+		</div>
+		</div>
+		</footer>
+		</html>
